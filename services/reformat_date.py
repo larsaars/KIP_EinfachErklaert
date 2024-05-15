c3cab1e584bc050b73367f3bffaee9bf0c84f9cb
Ben Reher <ben_quentin@hotmail.com>
2024-05-15 16:06:14 +0200

    refactor date format python script updated to work with directories that have mixed date formats

--- services/reformat_date.py
+++ services/reformat_date.py
@@ -9,14 +9,16 @@ def rename_directories():
             split_name = filename.split('-')
             if len(split_name) > 1:
                 date, title = split_name[0], '-'.join(split_name[1:])
-                try:
-                    datetime.strptime(date, '%Y-%m-%d')
-                    continue
-                except ValueError:
-                    day, month, year = date.split('.')
-                    new_date = '-'.join([year, month, day])
-                    new_name = '-'.join([new_date, title])
-                    os.rename(os.path.join(path, filename), os.path.join(path, new_name))
+                # Check if the date string is in the correct format
+                if '.' in date and len(date.split('.')) == 3:
+                    try:
+                        datetime.strptime(date, '%Y-%m-%d')
+                        continue
+                    except ValueError:
+                        day, month, year = date.split('.')
+                        new_date = '-'.join([year, month, day])
+                        new_name = '-'.join([new_date, title])
+                        os.rename(os.path.join(path, filename), os.path.join(path, new_name))
 
 
 rename_directories()
