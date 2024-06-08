f77f06aac4e815c4277ec50bfc4ed077273f253c
Simon Eiber <eiber.simon@web.de>
2024-06-08 12:20:29 +0200

    added CountVectorizer kwargs and smarter lowercasing

--- matchers/Ensemble/ArticleVectorizer.py
+++ matchers/Ensemble/ArticleVectorizer.py
@@ -13,8 +13,15 @@ except LookupError:
 
 
 class ArticleVectorizer(BaseEstimator, TransformerMixin):
-    def __init__(self, ngram_range=(1,1), convert_segmented_words=False, stop_words=True,
-                 non_alnum=True, capitalized_only=False, lowercase=True):
+    def __init__(self,
+                 ngram_range=(1,1),
+                 convert_segmented_words=False,
+                 stop_words=True,
+                 non_alnum=True,
+                 capitalized_only=False,
+                 lowercase=True,
+                 **kwargs):
+        
         self.ngram_range = ngram_range
         self.convert_segmented_words = convert_segmented_words
         self.stop_words = stop_words
@@ -22,16 +29,11 @@ class ArticleVectorizer(BaseEstimator, TransformerMixin):
         self.capitalized_only = capitalized_only
         self.lowercase = lowercase
         self.stop_words_set = set(stopwords.words('german')) if stop_words else set()
-        self.vectorizer = CountVectorizer(analyzer=self.analyzer)
+        self.vectorizer = CountVectorizer(analyzer=self.analyzer, **kwargs)
 
-        if lowercase and capitalized_only:
-            self.capitalized_only = False
 
     def generate_tokens(self, text: str) -> list[str]:
         tokens = word_tokenize(text, language='german')
-
-        if self.lowercase:
-            tokens = [token.lower() for token in tokens]
         
         if self.stop_words:
             tokens = [token for token in tokens if token.lower() not in self.stop_words_set]
@@ -52,6 +54,10 @@ class ArticleVectorizer(BaseEstimator, TransformerMixin):
 
         if self.capitalized_only:
             n_grams = get_ngrams_with_capitalized(n_grams)
+
+        if self.lowercase:
+            n_grams = [ngram.lower() for ngram in n_grams]
+            
         return n_grams
 
     def analyzer(self, text: str) -> list[str]:
@@ -70,14 +76,17 @@ class ArticleVectorizer(BaseEstimator, TransformerMixin):
 
 def get_ngrams_with_capitalized(ngrams: list[str]) -> list[str]:
     return [ngram for ngram in ngrams if any(word.istitle() for word in ngram.split())]
-    
+
+
 def is_segmented_word(word: str) -> bool:
     return '-' in word and word.count('-') > 0
+
 
 def convert_segmented_word(segmented_word: str) -> str:
     segments = segmented_word.split('-')
     segments = [segments[0].capitalize()] + [segment.lower() for segment in segments[1:]]
     return ''.join(segments)
+
 
 def filter_tokens(tokens: list[str]) -> list[str]:
     filtered_tokens = [
