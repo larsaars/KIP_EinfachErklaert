4a63b8dca6fc0d16b502956019a7755ae0cf5fec
lars <lars.lars.specht@gmail.com>
2024-06-11 16:26:35 +0200

    Update historic_news_scraper.py

--- scrapers/mdr/historic_news_scraper.py
+++ scrapers/mdr/historic_news_scraper.py
@@ -67,7 +67,7 @@ class MDRHistoricScraper(MDRBaseScraper):
 
             # save the hard article to the database
             # if one of the articles is newly scraped, the match is newly scraped
-            newly_scraped = newly_scraped or self.data_handler.save_article('hard', hard_metadata, hard_content, hard_html, download_audio=True)
+            newly_scraped = self.data_handler.save_article('hard', hard_metadata, hard_content, hard_html, download_audio=True) or newly_scraped
 
 
             # match the articles via simple matcher function
