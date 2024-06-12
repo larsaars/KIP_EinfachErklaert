9ef74794733dbf56fc312d670e23c9d65db68e42
FelixDigitalis <64135206+FelixDigitalis@users.noreply.github.com>
2024-06-12 10:22:35 +0200

    Scripts restored

---
+++ scripts/reformat_date.py
@@ -0,0 +1,24 @@
+import os
+from datetime import datetime
+
+
+def rename_directories():
+    path = os.getcwd()
+    for filename in os.listdir(path):
+        if os.path.isdir(os.path.join(path, filename)):
+            split_name = filename.split('-')
+            if len(split_name) > 1:
+                date, title = split_name[0], '-'.join(split_name[1:])
+                # Check if the date string is in the correct format
+                if '.' in date and len(date.split('.')) == 3:
+                    try:
+                        datetime.strptime(date, '%Y-%m-%d')
+                        continue
+                    except ValueError:
+                        day, month, year = date.split('.')
+                        new_date = '-'.join([year, month, day])
+                        new_name = '-'.join([new_date, title])
+                        os.rename(os.path.join(path, filename), os.path.join(path, new_name))
+
+
+rename_directories()
