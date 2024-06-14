ed7e372bdb014cfec4ad51c52fec6b213181381e
FelixDigitalis <64135206+FelixDigitalis@users.noreply.github.com>
2024-06-14 15:10:47 +0200

    cluster update

--- scripts/word_cluster.py
+++ scripts/word_cluster.py
@@ -1,73 +1,51 @@
 import os
 import subprocess
-import pandas as pd
-from wordcloud import WordCloud
+import sys
+from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plt
-from bs4 import BeautifulSoup
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
-# Get the git root directory
-git_root = (
-    subprocess.check_output(["git", "rev-parse", "--show-toplevel"], text=True)
+sys.path.append(
+    subprocess.check_output("git rev-parse --show-toplevel".split())
+    .decode("utf-8")
     .strip()
 )
 
+from datahandler.DataHandler import DataHandler
+
+# Get the git root directory
+git_root = subprocess.check_output(
+    ["git", "rev-parse", "--show-toplevel"], text=True
+).strip()
 base_dir = os.path.join(git_root, "data")
 
-data = []
+# Load the data
+dh = DataHandler("dlf")
+easy = dh.get_all("easy")
+hard = dh.get_all("hard")
+easy_articles = " ".join(easy["text"].tolist())
+hard_articles = " ".join(hard["text"].tolist())
 
-# Walk through the directory structure
-for source in os.listdir(base_dir):
-    source_dir = os.path.join(base_dir, source)
-    if os.path.isdir(source_dir):
-        for niveau in os.listdir(source_dir):
-            niveau_dir = os.path.join(source_dir, niveau)
-            if os.path.isdir(niveau_dir):
-                for article_dir in os.listdir(niveau_dir):
-                    article_path = os.path.join(niveau_dir, article_dir)
-                    if os.path.isdir(article_path):
-                        content_file = os.path.join(article_path, "Content.txt")
-                        
-                        if os.path.isfile(content_file):
-                            with open(content_file, 'r') as file:
-                                content = file.read()
-                            
-                            # Use BeautifulSoup to remove HTML tags
-                            soup = BeautifulSoup(content, "html.parser")
-                            text_content = soup.get_text()
-
-                            article_length = len(text_content.split())
-
-                            data.append({
-                                "source": source,
-                                "niveau": niveau,
-                                "article": article_dir,
-                                "content": text_content,
-                                "length": article_length
-                            })
-
-# Convert to DataFrame
-df = pd.DataFrame(data)
-
-# Filter articles from "dlf"
-dlf_df = df[df['source'] == 'mdr']
-
-# Separate easy and hard articles
-easy_articles = dlf_df[dlf_df['niveau'] == 'easy']['content'].str.cat(sep=' ')
-hard_articles = dlf_df[dlf_df['niveau'] == 'hard']['content'].str.cat(sep=' ')
 
 # Generate word clouds
 def generate_wordcloud(text, title, lvl):
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+    # German stopwords
+    german_stopwords = set(stopwords.words('german'))
+    wordcloud = WordCloud(width=800, height=400, background_color="white", stopwords=german_stopwords, max_words=30).generate(
+        text
+    )
     plt.figure(figsize=(10, 5))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.savefig(os.path.join(git_root, "documentation", "images", f'wordcloud{lvl}.png'))
+    plt.imshow(wordcloud, interpolation="bilinear")
     plt.title(title, fontsize=20)
-    plt.axis('off')
-    plt.show()
+    plt.axis("off")
+    wordcloud.to_file(os.path.join(git_root, "documentation", "images", f"wordcloud{lvl}.jpg"))
+    #plt.show()
+
 
 # Word cloud for easy articles
-generate_wordcloud(easy_articles, 'Wörter nach Häufigkeit bei MDR (leicht)', "easy")
+generate_wordcloud(easy_articles, "Wörter nach Häufigkeit bei Nachrichtenleicht", "easy")
 
 # Word cloud for hard articles
-generate_wordcloud(hard_articles, 'Wörter nach Häufigkeit bei MDR (normal)', "hard")
-
+generate_wordcloud(hard_articles, "Wörter nach Häufigkeit bei Deutcshlandfunk", "hard")
