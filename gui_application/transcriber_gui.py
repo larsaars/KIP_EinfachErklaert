b8a0c80df6742a0c36c1b046eb2f7bbf79b18437
Ben Reher <ben_quentin@hotmail.com>
2024-06-16 14:05:13 +0200

    updated for testing

--- gui_application/transcriber_gui.py
+++ gui_application/transcriber_gui.py
@@ -11,7 +11,7 @@ from datahandler.DataHandler import DataHandler
 
 
 app = Flask(__name__)
-app.secret_key = 'secret_key42'
+app.secret_key = 'secret_key'
 dh_dlf = DataHandler("dlf")
 dh_mdr = DataHandler("mdr")
 
