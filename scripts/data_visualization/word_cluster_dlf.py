7533fef70f229d6c8ffde5e0bec9ebd6b9d35947
FelixDigitalis <64135206+FelixDigitalis@users.noreply.github.com>
2024-06-18 12:04:28 +0200

    png -> jpg, one script for all images, other vis improvments

--- scripts/data_visualization/word_cluster_dlf.py
+++ scripts/data_visualization/word_cluster_dlf.py
@@ -23,31 +23,34 @@ git_root = subprocess.check_output(
 ).strip()
 base_dir = os.path.join(git_root, "data")
 
-# Load the data
-dh = DataHandler("dlf")
-easy = dh.get_all("easy")
-hard = dh.get_all("hard")
-easy_articles = " ".join(easy["text"].tolist())
-hard_articles = " ".join(hard["text"].tolist())
+def dlf_word_cluster():
+    # Load the data
+    dh = DataHandler("dlf")
+    easy = dh.get_all("easy")
+    hard = dh.get_all("hard")
+    easy_articles = " ".join(easy["text"].tolist())
+    hard_articles = " ".join(hard["text"].tolist())
 
-# Generate word clouds
-def generate_wordcloud(text, title, lvl):
-    # German stopwords
-    german_stopwords = set(stopwords.words('german'))
-    wordcloud = WordCloud(width=1600, height=800, background_color="white", stopwords=german_stopwords, max_words=100, collocations=False).generate(
-        text
-    )
-    plt.figure(figsize=(20, 10))
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.title(title, fontsize=20)
-    plt.axis("off")
-    file_path = os.path.join(git_root, "documentation", "images", "wordcluster", f"wordcluster_dlf_{lvl}.jpg")
-    plt.savefig(file_path, dpi=300, bbox_inches='tight')
-    # plt.show()
+    # Generate word clouds
+    def generate_wordcloud(text, title, lvl):
+        # German stopwords
+        german_stopwords = set(stopwords.words('german'))
+        wordcloud = WordCloud(width=1600, height=800, background_color="white", stopwords=german_stopwords, max_words=20, collocations=False).generate(
+            text
+        )
+        plt.figure(figsize=(20, 10))
+        plt.imshow(wordcloud, interpolation="bilinear")
+        plt.title(title, fontsize=50)
+        plt.axis("off")
+        file_path = os.path.join(git_root, "documentation", "images", f"wordcluster_dlf_{lvl}.jpg")
+        plt.savefig(file_path, dpi=300, bbox_inches='tight')
+        # plt.show()
 
-# Word cloud for easy articles
-generate_wordcloud(easy_articles, "Wörter nach Häufigkeit bei Nachrichtenleicht", "easy")
+    # Word cloud for easy articles
+    generate_wordcloud(easy_articles, "", "easy")
 
-# Word cloud for hard articles
-generate_wordcloud(hard_articles, "Wörter nach Häufigkeit bei Deutschlandfunk", "hard")
+    # Word cloud for hard articles
+    generate_wordcloud(hard_articles, "", "hard")
 
+if __name__ == "__main__":
+    dlf_word_cluster()
